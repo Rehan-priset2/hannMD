caa369a9dd3f4366d27c22aac05426255f8d4863
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-06-25 21:31:02 +0700

    Update test-bot.js

--- plugins/test-bot.js
+++ plugins/test-bot.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let caption = `*Gw tau lu cuman coba jaringan doang🗿*`
 
-conn.sendButton( m.chat, caption, `©️ ArullBotz`, `BISA Arull`, `.👍`, m)
+conn.sendButton( m.chat, caption, `©️ DanzBot-MD`, `BISA Danzz`, `.Ntaps👍`, m)
 
        }
        
